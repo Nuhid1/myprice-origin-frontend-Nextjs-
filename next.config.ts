3e8f5e8ce6c7ca171a434eda727764699bce0c03
Nuhid1 <nuhid96@Gmail.com>
2026-05-25 00:57:08 +0600

    seo fixed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
-}
+  async redirects() {
+    return [{ source: "/home", destination: "/", permanent: true }];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
